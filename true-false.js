066355a0675aa35cdb7010d848b4f47be908d325
touhidcodes <touhidcodes@gmail.com>
2022-11-11 21:43:05 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+let name = null;
+if (name) {
+  console.log("The condition is True");
+} else {
+  console.log("The condition is False");
+}
+
+// Get True Output by
+//True
+//Truthy
+//'0', ' ', {}, []
+
+// Get False Output by
+//False
+// 0
+//""
+//undefined
+//null
+//NaN
