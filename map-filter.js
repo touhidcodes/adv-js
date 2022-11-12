e49142ae1987765a5d8972606346bd97cfcac914
touhidcodes <touhidcodes@gmail.com>
2022-11-12 22:25:00 +0600

    map filter

--- map-filter.js
+++ map-filter.js
@@ -1,6 +1,6 @@
-//MAP works on array
+//MAP works on element, index, array
 
-// Square numbers by loop
+//Square numbers by loop
 const numbers = [2, 4, 6, 7, 8, 5, 9];
 const output = [];
 
@@ -9,4 +9,19 @@ for (let i = 0; i < numbers.length; i++) {
   const result = element * element;
   output.push(result);
 }
+
 console.log(output);
+
+//MAP numbers by function
+function squre(element) {
+  return element * element;
+}
+
+const display = squre(numbers);
+
+//Square numbers by MAP
+const result = numbers.map(function (element) {
+  return element * element;
+});
+
+console.log(result);
