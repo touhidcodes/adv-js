21188a74bcb67e2bf5b045e9fd1d0bbeaeef2f29
touhidcodes <touhidcodes@gmail.com>
2022-11-13 11:59:36 +0600

    array split

---
+++ array-split.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array);
+
+//Split array
+const part = array.slice(2, 5);
+console.log(part);
+
+//Remove array
+const remove = array.splice(3, 6);
+console.log(remove);
+
+//Join array
+const join = array.join(" ami ");
+console.log(join);
